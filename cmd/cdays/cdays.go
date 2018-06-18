478efff2a1fcf47e0a7c48cd1d7a8e21db6fab75
Yuri Maltsev <yurii.maltsev@xing.com>
2018-06-18 16:58:08 +0200

    add vendors

--- cmd/cdays/cdays.go
+++ cmd/cdays/cdays.go
@@ -35,12 +35,15 @@ func main() {
 
 	var blServer, diagServer http.Server
 
+	srvErrs := make(chan error, 2)
+
 	go func() {
 		blServer = http.Server{
 			Addr:    ":" + port,
 			Handler: routing.NewBLRouter(),
 		}
-		log.Fatal(blServer.ListenAndServe())
+		err := blServer.ListenAndServe()
+		srvErrs <- err
 	}()
 
 	go func() {
@@ -48,7 +51,8 @@ func main() {
 			Addr:    ":" + diagPort,
 			Handler: routing.NewDiagnosticsRouter(),
 		}
-		log.Fatal(diagServer.ListenAndServe())
+		err := diagServer.ListenAndServe()
+		srvErrs <- err
 	}()
 
 	interrupt := make(chan os.Signal, 1)
@@ -56,6 +60,8 @@ func main() {
 	select {
 	case killSignal := <-interrupt:
 		log.Printf("Got %s. Stopping...", killSignal)
+	case err := <-srvErrs:
+		log.Printf("Got a server error: %s", err)
 	}
 
 	{
